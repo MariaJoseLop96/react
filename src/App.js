3694195e611c102546080763f65f277f261c3d20
Maria Jose Loperfido <majoloperfido@hotmail.com>
2022-10-20 23:15:26 -0300

    modificacion de item desafio 1

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import './estilos/estilos.css'
 import {Navbar} from './componentes/Navbar'
-
 import React from 'react'
-import {CartWidget} from './componentes/CartWidget'
+import {ItemList} from './componentes/ItemList'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+
+
+      <ItemList greeting = "desafio 1" />
       
     </div>
   );
---
+++ src/componentes/ItemList.jsx
@@ -0,0 +1,10 @@
+
+ export const ItemList = ({greeting}) =>{
+    return(
+        <div className='greeting'>
+            <h1>{greeting}</h1>
+        </div>
+    )
+}
+
+export default ItemList;
